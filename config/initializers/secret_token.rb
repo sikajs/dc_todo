43eb240bbd4d623fe157632fd9fdfdd0590155f2
Jason Shen <sikajs@gmail.com>
2013-12-10 14:30:04 +0700

    Use rails 4.0.2 to create the skeleton app and init git repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DcTodo::Application.config.secret_key_base = 'e00f136d72b3b3d50399bf2fb828ba59c75ebe75ae41f3d3434665691cf855f0f395f6a4784214d941943cca62eed66001d5005839dfe2348bfbaf37b6b1713f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DcTodo::Application.config.session_store :cookie_store, key: '_dc_todo_session'
